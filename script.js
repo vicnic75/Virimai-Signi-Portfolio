479ed6b9792a83ec5aeb3d2664cb87808678480d
Virimai Signi <97977143+vicnic75@users.noreply.github.com>
2024-02-13 14:44:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function cont(){
 document.querySelector('#main-sect').innerHTML=`<section class="sct2">
-<h1 class="company-heading"><span class="africa zoom">CLOUD</span class='cloud zoom'><span class="africa" class="zoom"></span><span class="africa zoom"> AFRICA</span> <span id="parts-spares"> <span class="cloud"><br>PARTS</span> <span class="cloud">SPARES</span></h1>
-<p class="par"><i>Cloud Africa 
-    , Keeeping You On The Road</i>
+<h1 class="company-heading"><span class="africa zoom">Virimai</span class='cloud zoom'><span class="africa" class="zoom"></span><span class="africa zoom"> Signi</span> <span id="parts-spares"> <span class="cloud"><br>----</span> <span class="cloud">----</span></h1>
+<p class="par"><i>Salesperson</i>
 </p>
-<p><i style="font-size:15px;">email us @<br></i><button><a target='blank'href="mailto:partsspares@clouda.co.zw">partsspares@clouda.co.zw</a></button></p>
+<p><i style="font-size:15px;">email us @<br></i><button><a target='blank'href="mailto:vicnic75@gmail.com ">vicnic75@gmail.com</a></button></p>
 <p><i style="font-size:15px;">call us @<br></i><button><a target='blank'href="tel:+263774816199">+263774816199</a></button></p>
 </section>`
 }
